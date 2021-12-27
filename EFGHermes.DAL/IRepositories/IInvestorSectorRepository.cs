79981f04801b62d14d4ebd14655b7943a42ee02c
Mina Youssef <MinaY@Microwize.com>
2021-12-27 20:21:46 +0200

    Add InvestorSectorRepository

---
+++ EFGHermes.DAL/IRepositories/IInvestorSectorRepository.cs
@@ -0,0 +1,12 @@
+﻿using EFGHermes.Data.DAL.IPersistance;
+using EFGHermes.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFGHermes.DAL.IRepositories
+{
+    public interface IInvestorSectorRepository : IRepository<InvestorSector>
+    {
+    }
+}
