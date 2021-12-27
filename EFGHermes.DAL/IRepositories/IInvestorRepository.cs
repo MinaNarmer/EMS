687f75eef8ff811e49247f3c1d1ceb1deed493e5
Mina Youssef <MinaY@Microwize.com>
2021-12-27 20:16:21 +0200

    Add Investor repository

---
+++ EFGHermes.DAL/IRepositories/IInvestorRepository.cs
@@ -0,0 +1,9 @@
+﻿using EFGHermes.Data.DAL.IPersistance;
+using EFGHermes.Data.Models;
+
+namespace EFGHermes.DAL.IRepositories
+{
+   public interface IInvestorRepository : IRepository<Investor>
+    {
+    }
+}
---
+++ EFGHermes.DAL/Repositories/InvestorRepository.cs
@@ -0,0 +1,13 @@
+﻿using EFGHermes.DAL.IRepositories;
+using EFGHermes.Data.DAL.IPersistance;
+using EFGHermes.Data.DAL.Persistance;
+using EFGHermes.Data.Models;
+
+namespace EFGHermes.DAL.Repositories
+{
+    public class InvestorRepository : RepositoryBase<Investor>, IInvestorRepository
+    {
+        public InvestorRepository(IDbFactory dbFactory)
+        : base(dbFactory) { }
+    }
+}
